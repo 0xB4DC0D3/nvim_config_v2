03422782a49eb2451d3171398e067381889c3c05
Enigma <50676974+0xB4DC0D3@users.noreply.github.com>
2023-09-27 22:31:00 +0300

    fixed error with codelldb package in mason for an installation from scratch & macos

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -8,30 +8,38 @@ return {
     vim.fn.sign_define("DapLogPoint", { text = ".>", texthl = "DiagnosticInfo" })
 
     local dap = require("dap")
-    local codelldb = require('mason-registry').get_package('codelldb'):get_install_path() .. '/extension/adapter/codelldb.exe'
+    local mason_registry = require("mason-registry")
 
-    dap.adapters.codelldb = {
-      type = 'server',
-      port = '${port}',
-      executable = {
-        command = codelldb,
-        args = {
-          "--port", "${port}",
-          "--settings", '{ "showDisassembly": "never" }'
+    if mason_registry.has_package("codelldb") then 
+      local codelldb = mason_registry.get_package('codelldb'):get_install_path() .. '/extension/adapter/codelldb.exe'
+
+      if vim.fn.has("mac") == 1 then
+        codelldb = mason_registry.get_package('codelldb'):get_install_path() .. '/extension/adapter/codelldb'
+      end
+
+      dap.adapters.codelldb = {
+        type = 'server',
+        port = '${port}',
+        executable = {
+          command = codelldb,
+          args = {
+            "--port", "${port}",
+            "--settings", '{ "showDisassembly": "never" }'
+          },
+        }
+      }
+
+      dap.configurations.rust = {
+        {
+          name = "Rust debug",
+          type = "codelldb",
+          request = "launch",
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
         },
       }
-    }
-
-    dap.configurations.rust = {
-      {
-        name = "Rust debug",
-        type = "codelldb",
-        request = "launch",
-        program = function()
-          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
-        end,
-        cwd = '${workspaceFolder}',
-      },
-    }
+    end
   end
 }
