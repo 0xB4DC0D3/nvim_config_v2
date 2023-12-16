df245f2cd9c1995a40656658f108178ac1ee0656
Enigma <mykola.siheti@seezona.com>
2023-12-16 20:16:22 +0200

    chore: updated config

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -51,52 +51,6 @@ return {
           }
         }
       end
-
-      for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
-        dap.configurations[language] = {
-          {
-            type = "pwa-node",
-            request = "launch",
-            name = "Launch file",
-            program = "${file}",
-            cwd = "${workspaceFolder}",
-          },
-          {
-            type = "pwa-node",
-            request = "attach",
-            name = "Attach",
-            processId = require'dap.utils'.pick_process,
-            cwd = "${workspaceFolder}",
-          },
-          {
-            name = "Launch debug (TypeScript, JavaScript)",
-            type = "pwa-chrome",
-            request = "launch",
-            port = 9222,
-            url = "http://localhost:3000",
-            webRoot = "${workspaceFolder}",
-            protocol = "inspector",
-          },
-          {
-            type = "pwa-chrome",
-            request = "attach",
-            port = "${port}",
-            name = "Attach debug (TypeScript, JavaScript)",
-            processId = require'dap.utils'.pick_process,
-            cwd = "${workspaceFolder}",
-          }
-        }
-      end
     end
   },
-  {
-    "msxdev/nvim-dap-vscode-js",
-    build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-    config = function()
-      require("dap-vscode-js").setup({
-        debugger_path = vim.fn.stdpath('data') .. '/mason/packages/js-debug-adapter',
-        debugger_cmd = { 'js-debug-adapter' },
-      })
-    end
-  }
 }
