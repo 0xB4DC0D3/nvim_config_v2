f1bfe128eb2b922f466a841235cb2e12d9e3dcd2
Enigma <mykola.siheti@seezona.com>
2024-01-03 23:58:50 +0200

    chore: updated config

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -14,7 +14,7 @@ return {
         vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
         vim.fn.sign_define("DiagnosticSignHint", { text = "󰋼", texthl = "DiagnosticSignHint" })
         vim.fn.sign_define("DiagnosticSignInfo", { text = "󰋼", texthl = "DiagnosticSignInfo" })
-      end
+      end,
     })
   end
 }
